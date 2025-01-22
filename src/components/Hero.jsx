feaa43609dabfcfe52b465e22ab9d91a57085ecc
NocturnalK29 <kunalballal29@gmail.com>
2025-01-22 22:03:29 +0530

    updates-2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import profilePic from "../assets/raviKumarProfile.webp"
+import profilePic from "../assets/KunalProfile.png"
 
 import {HERO_CONTENT} from "../constants/index"
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { FaGithub, FaInstagram, FaLinkedin} from "react-icons/fa"
-import logo from "../assets/raviKumarLogo.webp"
+import logo from "../assets/NK_logo_1.png"
 import { FaSquareXTwitter } from "react-icons/fa6"
 
 const Navbar = () => {
@@ -7,7 +7,7 @@ const Navbar = () => {
     <nav className="flex items-center justify-between py-6">
         <div className="flex flex-shrink-0 items-center">
           <a href="/" aria-label="Home">
-            <img src={logo} className="mx-2" width={50} height={33} alt="Logo" />
+            <img src={logo} className="mx-2" width={60} height={33} alt="Logo" />
           </a>
         </div>
         <div className="m-8 flex items-center justify-center gap-4 text-2xl">
